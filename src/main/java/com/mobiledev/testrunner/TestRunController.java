bfdaace0ad67b541e3824a49b644970459c3732d
José Italo Gouveia Soares <italo_2512@hotmail.com>
2025-03-17 00:26:40 -0300

    Adding the data structure that we are supposing to use on the controller
    - A List of strings that will e the workers.
    - A Map of String, and the TestRunStatus object that will be an Concurrent Hash Map(that is thread safe, the key it's supposed to be the runId).
    - ExecutorService with the Executors.newCachedThreadPool, used for create a thread pool to handle our tasks assynchronously.

--- src/main/java/com/mobiledev/testrunner/TestRunController.java
+++ src/main/java/com/mobiledev/testrunner/TestRunController.java
@@ -2,11 +2,20 @@ package com.mobiledev.testrunner;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @RestController
 @RequestMapping("/api/v1")
 public class TestRunController {
 
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(TestRunController.class);
+
+    private final List<String> workers = new ArrayList<>(Arrays.asList("worker1", "worker2", "worker3"));
+    private final Map<String, TestRunStatus> testRuns = new ConcurrentHashMap<>();
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     // Health endpoint
     @GetMapping("/health")
