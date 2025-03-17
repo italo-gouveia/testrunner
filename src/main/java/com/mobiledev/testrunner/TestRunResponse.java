7c1d846d98ce3f84884d96396c3a44925e361d53
José Italo Gouveia Soares <italo_2512@hotmail.com>
2025-03-17 00:00:40 -0300

    Added initial response object

---
+++ src/main/java/com/mobiledev/testrunner/TestRunResponse.java
@@ -0,0 +1,10 @@
+package com.mobiledev.testrunner;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestRunResponse {
+    private String runId;
+}
