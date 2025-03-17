68f5e370d6a0ea8ead4b49f447db163f764ebf99
José Italo Gouveia Soares <italo_2512@hotmail.com>
2025-03-17 00:32:51 -0300

    Created a method and endpoint to submit a test run
    - It Returns a TestRunResponse object
    - Using the UUID.randomUUID to generate the runId
    - Add to out concurrent hash map of testRuns an new testRuns with the id generated, and a new object that pass the status as QUEUED, and the request data from the TestRunRequest object(fields apkUrl, testScript, timeout)
    - log the information
    - created an todo to implement the executeTestRun method
    - it should e submited to the executorService

--- src/main/java/com/mobiledev/testrunner/TestRunController.java
+++ src/main/java/com/mobiledev/testrunner/TestRunController.java
@@ -1,6 +1,8 @@
 package com.mobiledev.testrunner;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -21,6 +23,22 @@ public class TestRunController {
     @GetMapping("/health")
     public boolean health() {
         return true;
+    }
+
+    // Submit a test run
+    @PostMapping("/test-runs")
+    public TestRunResponse submitTestRun(@RequestBody TestRunRequest request) {
+        String runId = UUID.randomUUID().toString();
+        testRuns.put(runId, new TestRunStatus(
+                "QUEUED",
+                request.getApkUrl(),
+                request.getTestScript(),
+                request.getTimeout()
+        ));
+        logger.info("Test run submitted: " + runId);
+        //TODO: Implement the executeTestRun method
+        //executorService.submit(() -> executeTestRun(runId));
+        return new TestRunResponse(runId);
     }
 
 }
